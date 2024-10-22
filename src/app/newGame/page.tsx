88f7fbb694835a96beed95a0bb095c91c05af9dd
Camilo A <camiloab01@gmail.com>
2024-10-21 22:45:17 -0500

    Add components

--- src/app/newGame/page.tsx
+++ src/app/newGame/page.tsx
@@ -1,12 +1,27 @@
 import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 sm:items-start">
         <h1 className="text-4xl text-center">New Game 🎲</h1>
-        <div>
-          <Input />
+        <div className="flex flex-col gap-4">
+          <DropdownMenu>
+            <DropdownMenuTrigger>Stake of the round</DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Beer 🍺</DropdownMenuItem>
+              <DropdownMenuItem>Malort shot 🥃</DropdownMenuItem>
+              <DropdownMenuItem>Rum 🍹</DropdownMenuItem>
+              <DropdownMenuItem>Virgin Piña Colada 🍍</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Input placeholder="Your name" />
         </div>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -15,15 +30,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            👉🏻 Join a game
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            👉🏻 Start a new game
+            👉🏻 Create game
           </a>
         </div>
       </main>
